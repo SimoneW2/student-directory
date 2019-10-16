16e421c0ad1718a457162d01b3ed4775c3afba23
Simone <simonewlkr@hotmail.com>
2019-10-16 22:01:57 +0100

    each do iteration used on student name array

--- directory.rb
+++ directory.rb
@@ -1,31 +1,23 @@
 
-#let's put all students into an array
+# let's put all students into an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+	"Dr. Hannibal Lecter",
+	"Darth Vader",
+	"Nurse Ratched",
+	"Michael Corleone",
+	"Alex DeLarge",
+	"The Wicked Witch of the West",
+	"Terminator",
+	"Freddy Krueger",
+	"The Joker",
+	"Joffrey Baratheon",
+	"Norman Bates"
 ]
-
-#add and then print them
+# and then print them
 puts "The students of Villains Academy"
 puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[10]
-# finally, we print the total number of students
+students.each do |student|
+	puts student
+end
+# finally, we print the total
 puts "Overall, we have #{students.count} great students"
